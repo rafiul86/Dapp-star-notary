1b9d996962eea3100f4be1ff87efe925e8330a22
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-06-16 19:30:30 +0600

    transfer function added

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -72,6 +72,12 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+       
+        require(( msg.sender == ownerOf(_tokenId1) ||  msg.sender == ownerOf(_tokenId2)), "You are not the owner");
+
+        _transferFrom(ownerOf(_tokenId1), ownerOf(_tokenId2), _tokenId1);
+
+        _transferFrom(ownerOf(_tokenId2), ownerOf(_tokenId1), _tokenId2);
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
