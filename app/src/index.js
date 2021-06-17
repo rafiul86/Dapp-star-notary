336dbe584c503eced12459df532b4516ea723184
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-06-17 12:34:03 +0600

    pandora star ERC721 Dapp completed

--- app/src/index.js
+++ app/src/index.js
@@ -41,7 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const lookId = document.getElementById("lookid").value;
+    await lookUptokenIdToStarInfo(lookId).send({from: this.account});
+    App.setStatus("Your searched Star Owner is " + this.account + ".");
   }
 
 };
